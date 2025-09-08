2c69be2352d5fa37bcc757e8b0f9ee8d30b1355c
hoangpv-zigexn <hoangpv@zigexn.vn>
2025-09-08 09:04:13 +0700

    chapter 3 static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 App::Application.routes.draw do
+  get "static_pages/home" => "static_pages#home"
+  get "static_pages/help" => "static_pages#help"
+  get "static_pages/about" => "static_pages#about"
+
   resources :microposts
 
 
