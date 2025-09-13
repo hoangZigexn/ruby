31d7be4cc7af6da49720d5702ded6ce4508191e3
hoangpv-zigexn <hoangpv@zigexn.vn>
2025-09-13 23:05:27 +0700

    chapter12 reset password

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,4 +5,9 @@ class UserMailer < ActionMailer::Base
     @user = user
     mail to: user.email, subject: "Account activation"
   end
+
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Password reset"
+  end
 end
