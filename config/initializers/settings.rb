a68f3b4466602273b5c6536ad4ec11df241d99fb
hoangpv-zigexn <hoangpv@zigexn.vn>
2025-09-15 15:07:10 +0700

    fix comment

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,4 +1,3 @@
 class Settings < Settingslogic
-    source Rails.root.join("config", "settings", "#{Rails.env}.yml")
-  end
-  
+  source Rails.root.join("config", "settings", "#{Rails.env}.yml")
+end
