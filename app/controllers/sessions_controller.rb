4175fee8f71dbf07ef67f5263bd47dd00493eb8c
hoangpv-zigexn <hoangpv@zigexn.vn>
2025-09-15 13:21:22 +0700

    chapter8 fix query filter

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.where(email: params[:session][:email].downcase).first
+    user = User.find_by_email(params[:session][:email].downcase)
+
     if user && user.authenticate(params[:session][:password])
       log_in user
       flash[:success] = "Welcome back, #{user.name}!"
