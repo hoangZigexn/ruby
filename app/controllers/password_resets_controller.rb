49e3a96abc911bc349c66b82c7fdc629b10f498b
hoangpv-zigexn <hoangpv@zigexn.vn>
2025-09-16 08:42:32 +0700

    fix indent

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -39,28 +39,28 @@ class PasswordResetsController < ApplicationController
 
   private
 
-    def user_params
-      params[:user]
-    end
+  def user_params
+    params[:user]
+  end
 
-    def get_user
-      @user = User.find_by_email(params[:email].downcase)
-      @user.reset_token = params[:id] if @user
-    end
+  def get_user
+    @user = User.find_by_email(params[:email].downcase)
+    @user.reset_token = params[:id] if @user
+  end
 
-    # Confirms a valid user.
-    def valid_user
-      unless (@user && @user.activated? &&
-              @user.authenticated?(:reset, params[:id]))
-        redirect_to login_url
-      end
+  # Confirms a valid user.
+  def valid_user
+    unless (@user && @user.activated? &&
+            @user.authenticated?(:reset, params[:id]))
+      redirect_to login_url
     end
+  end
 
-    # Checks expiration of reset token.
-    def check_expiration
-      if @user.password_reset_expired?
-        flash[:danger] = "Password reset has expired."
-        redirect_to new_password_reset_url
-      end
+  # Checks expiration of reset token.
+  def check_expiration
+    if @user.password_reset_expired?
+      flash[:danger] = "Password reset has expired."
+      redirect_to new_password_reset_url
     end
+  end
 end
