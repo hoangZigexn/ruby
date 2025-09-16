fe31cdf2d99ab94d5683676c52dfd3be28c820f6
hoangpv-zigexn <hoangpv@zigexn.vn>
2025-09-16 16:08:26 +0700

    fix comment

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -50,17 +50,15 @@ class PasswordResetsController < ApplicationController
 
   # Confirms a valid user.
   def valid_user
-    unless (@user && @user.activated? &&
-            @user.authenticated?(:reset, params[:id]))
-      redirect_to login_url
-    end
+    return if @user&.activated? && @user.authenticated?(:reset, params[:id])
+    redirect_to login_url
   end
 
   # Checks expiration of reset token.
   def check_expiration
-    if @user.password_reset_expired?
-      flash[:danger] = "Password reset has expired."
-      redirect_to new_password_reset_url
-    end
+    return unless @user.password_reset_expired?
+  
+    flash[:danger] = "Password reset has expired."
+    redirect_to new_password_reset_url
   end
 end
